4aaf734daa91aa2dff49f57cd2d1aec69871f244
graymclaney <gamclaney@crimson.ua.edu>
2025-10-02 17:30:23 -0500

    UPDATE3

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+// Serve files from wwwroot
+app.UseStaticFiles();
+
+// Optional: if you want hitting "/" to load a default file automatically.
+// app.UseDefaultFiles(); // looks for index.html, default.html, etc.
 
 app.Run();
